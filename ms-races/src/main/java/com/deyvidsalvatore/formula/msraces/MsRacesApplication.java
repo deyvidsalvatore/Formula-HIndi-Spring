96d00ea17eda88614072ca4dcc709926bd23fe31
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-09-10 19:49:00 -0300

    chore(ms-races): Enabling Feign Client

--- ms-races/src/main/java/com/deyvidsalvatore/formula/msraces/MsRacesApplication.java
+++ ms-races/src/main/java/com/deyvidsalvatore/formula/msraces/MsRacesApplication.java
@@ -2,8 +2,10 @@ package com.deyvidsalvatore.formula.msraces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MsRacesApplication {
 
     public static void main(String[] args) {
