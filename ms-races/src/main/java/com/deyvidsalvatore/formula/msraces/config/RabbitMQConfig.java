7affcc9f96d41417b41eba9ac694348785938452
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2023-09-10 21:08:32 -0300

    feat(ms-races): RabbitMQ Configuration and Queue

---
+++ ms-races/src/main/java/com/deyvidsalvatore/formula/msraces/config/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package com.deyvidsalvatore.formula.msraces.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setHost("localhost");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        return connectionFactory;
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate() {
+        return new RabbitTemplate(connectionFactory());
+    }
+
+    @Bean
+    public Queue raceResultQueue() {
+        return new Queue("race-result-queue");
+    }
+}
